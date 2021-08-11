a9d35d7ef5b26e30893fc81c2daee37cb257447e
Niels Koster <n.koster@portavita.eu>
2021-08-12 01:19:15 +0200

    ATM-2771: Pick functions from functions directory + Use node cluser and fork one worker + Better logging

--- server.js
+++ server.js
@@ -1,42 +1,69 @@
 const express = require('express')
-const app = express()
+const cluster = require('cluster')
+const fs = require('fs')
+
 const API_PORT = process.env.API_PORT || 3030
 
-app.use(express.json())
-
-app.use((_, res, next) => {
-  const start = process.hrtime()
-  res.on('close', _ => {
-      const durationInMilliseconds = getDurationInMilliseconds(start)
-      console.log(` ${durationInMilliseconds.toLocaleString()} ms`)
-  })
-  next()
-})
-
-const getDurationInMilliseconds = start => {
-  const NS_PER_SEC = 1e9
-  const NS_TO_MS = 1e6
-  const diff = process.hrtime(start)
-  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
-}
-
-const functions = [
-  { name: 'test', counter: 0 },
-  { name: 'aap', counter: 0 }
-]
-
-for (let f = 0; f < functions.length; f++) {
-  app.post(`/function/${functions[f].name}`, ((req, res) => {
-    const func = require(`./${functions[f].name}`)
-    functions[f].counter += 1
-    process.stdout.write(`Invoking function "${functions[f].name}" (${functions[f].counter})`)
-    try {
-      func(req.body, res)
-    } catch(err) {
-      console.log(err.message)
-      res.status(500).send()
+const functions = fs.readdirSync(__dirname + '/functions')
+  .map(f => {
+    return {
+      name: f,
+      counter: 0
     }
-  }))
+  })
+
+if (!functions) {
+  console.log(`No functions found in ${__dirname}/functions/`)
+  process.exit(3)
 }
 
-app.listen(API_PORT, _ => console.log(`RunFaaS running at port ${API_PORT}`))
+if (cluster.isMaster) {
+  cluster.fork()
+  cluster.on('exit', (worker, code, signal) => {
+    if (signal) console.log(`signal ${signal} received`)
+    console.log(`worker ${worker.id} with PID ${worker.process.pid} terminated with exit code ${code}`)
+    cluster.fork()
+  })
+}
+
+if (cluster.isWorker) {
+
+  const app = express()
+
+  app.use(express.json())
+  
+  app.use((_, res, next) => {
+    const start = process.hrtime()
+    res.on('close', _ => {
+        const durationInMilliseconds = getDurationInMilliseconds(start)
+        console.log(`--- Function finished after ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+    next()
+  })
+  
+  const getDurationInMilliseconds = start => {
+    const NS_PER_SEC = 1e9
+    const NS_TO_MS = 1e6
+    const diff = process.hrtime(start)
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+  }
+  
+  for (let f = 0; f < functions.length; f++) {
+    app.post(`/function/${functions[f].name}`, ((req, res) => {
+      const func = require(`./functions/${functions[f].name}/index`)
+      functions[f].counter += 1
+      console.log(`--- Invoking function "${functions[f].name}" (${functions[f].counter})`)
+      try {
+        func(req.body, res)
+      } catch(err) {
+        console.log(err.message)
+        res.status(500).send()
+      }
+    }))
+  }
+  
+  app.listen(API_PORT, _ => console.log(`RunFaaS running at port ${API_PORT}\n${functions.length} function${
+    functions.length != 1 ? 's' : ''
+  } loaded`))
+  
+}
