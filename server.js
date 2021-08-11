c344422e860a7289a52bc79682bf200f1b12c12d
Niels Koster <n.koster@portavita.eu>
2021-08-11 02:34:20 +0200

    Invocation counter + 22 lines code

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require('express')
 const app = express()
 const API_PORT = process.env.API_PORT || 3030
-
 app.use(express.json())
-
 const functions = [
-  'test',
-  'aap'
+  { name: 'test', counter: 0 },
+  { name: 'aap', counter: 0 }
 ]
-
-functions.forEach(f => {
-  app.post(`/function/${f}`, ((req, res) => {
-    const func = require(`./${f}`)
+for (let f = 0; f < functions.length; f++) {
+  app.post(`/function/${functions[f].name}`, ((req, res) => {
+    const func = require(`./${functions[f].name}`)
     try {
+      functions[f].counter += 1
+      console.log(`Invoking function "${functions[f].name}" (${functions[f].counter})`)
       func(req.body, res)
-      console.log(`Invoked function "${f}"`)
     } catch(err) {
       console.log(err.message)
       res.status(500).send()
     }
   }))
-})
-
-app.listen(API_PORT, _ =>
-  console.log(`RunFaaS running at port ${API_PORT}`))
+}
+app.listen(API_PORT, _ => console.log(`RunFaaS running at port ${API_PORT}`))
