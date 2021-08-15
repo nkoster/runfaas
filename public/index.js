d51596ac4ffd10831e949ca05eec77a57c7a89de
Niels Koster <n.koster@portavita.eu>
2021-08-15 03:19:14 +0200

    Add admin page with websockets

---
+++ public/index.js
@@ -0,0 +1,26 @@
+const div = document.querySelector('div')
+let socket
+
+div.style.fontFamily = 'monospace'
+div.innerHTML = '<pre style="text-shadow:0px 0px 5px black">\n' + 
+' ____  _  _  __ _  ____  __    __   ____\n' +
+'(  _ \\/ )( \\(  ( \\(  __)/ _\\  / _\\ / ___)\n' +
+' )   /) \\/ (/    / ) _)/    \\/    \\\\___ \\\n' +
+'(__\\_)\\____/\\_)__)(__) \\_/\\_/\\_/\\_/(____/\n\n</pre>'
+
+
+const connect = _ => {
+    socket = new WebSocket('ws://localhost:3030')
+    socket.addEventListener('message', evt => {
+        div.innerHTML += `${evt.data}`
+    })
+    socket.onclose = _ => {
+        console.log('Socket closed. Reconnect in 2 seconds.')
+        setTimeout(_ => {
+            div.innerHTML += '<span style="color:blue">' + new Date(Date.now()).toString().replace(/\((.+)\)/, '') + '--- Reload</span><br>\n'
+            connect()
+        }, 2000)
+    }    
+}
+
+connect()
