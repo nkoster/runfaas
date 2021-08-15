2f13833e31b5af8c3cc507680edc729a22a3d7f2
Niels Koster <n.koster@portavita.eu>
2021-08-15 13:59:16 +0200

    Simplify log line + Convert to unix eol

--- public/index.js
+++ public/index.js
@@ -2,7 +2,9 @@ const div = document.querySelector('div')
 let socket
 
 div.style.fontFamily = 'monospace'
-div.innerHTML = '<pre style="text-shadow:0px 0px 5px black">\n' + 
+div.style.fontSize = '1.3em'
+div.style.margin = '30px'
+div.innerHTML = '<pre style="text-shadow:0px 0px 5px black">\n' +
 ' ____  _  _  __ _  ____  __    __   ____\n' +
 '(  _ \\/ )( \\(  ( \\(  __)/ _\\  / _\\ / ___)\n' +
 ' )   /) \\/ (/    / ) _)/    \\/    \\\\___ \\\n' +
@@ -12,15 +14,15 @@ div.innerHTML = '<pre style="text-shadow:0px 0px 5px black">\n' +
 const connect = _ => {
     socket = new WebSocket('ws://localhost:3030')
     socket.addEventListener('message', evt => {
-        div.innerHTML += `${evt.data}`
+        div.innerHTML += `${evt.data.replace('--- Invoking f', 'F').replace(' Function finished in', '')}`
     })
     socket.onclose = _ => {
         console.log('Socket closed. Reconnect in 2 seconds.')
         setTimeout(_ => {
-            div.innerHTML += '<span style="color:blue">' + new Date(Date.now()).toString().replace(/\((.+)\)/, '') + '--- Reload</span><br>\n'
+            div.innerHTML += '<span style="color:blue">' + new Date(Date.now()).toString().replace(/\((.+)\)/, '') + 'Reload</span><br>\n'
             connect()
         }, 2000)
-    }    
+    }
 }
 
 connect()
