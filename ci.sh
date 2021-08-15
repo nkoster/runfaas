2c864f609d7488f8b4b0e0f3765d29d8473c0586
Niels Koster <n.koster@portavita.eu>
2021-08-15 21:10:33 +0200

    Add public/indexjs to ci

--- ci.sh
+++ ci.sh
@@ -5,4 +5,10 @@ do
     scp server.js niels:src/node/runfaas/
     ssh niels 'systemctl --user stop runfaas.service'
     ssh niels 'systemctl --user start runfaas.service'
+done &
+
+while inotifywait -e close_write public/index.js
+do
+    scp public/index.js niels:src/node/runfaas/public/
+    ssh niels "sed -i 's/ws:\/\/localhost:3030/wss:\/\/faas.fhirstation.net/' src/node/runfaas/public/index.js"
 done
