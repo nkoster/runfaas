c6fd9e8d836d8ef1e2eff42bc1a2317f544f82c9
Niels Koster <n.koster@portavita.eu>
2021-08-15 03:19:14 +0200

    ATM-2771: Add admin page with websockets

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const functionsPath = process.env.FUNCTIONS_PATH || './functions'
 const log = console.log.bind(console)
 const chokidar = require('chokidar')
 const express = require('express')
+const app = express()
 const cluster = require('cluster')
+const open = require('open')
 const fs = require('fs')
 const jwt = require('jsonwebtoken')
+const ws = require('ws')
+const wss = new ws.Server({ noServer: true })
+const sockets = new Map()
 
 const API_PORT = process.env.API_PORT || 3030
 
@@ -60,11 +65,23 @@ if (cluster.isMaster) {
 
   log(`--- RunFaaS running at port ${API_PORT}`)
 
+  open(`http://localhost:${API_PORT}`)
+
 }
 
 if (cluster.isWorker) {
 
-  const app = express()
+  app.use((req, res, next) => {
+    if (req.method === 'POST') return next()
+    const auth = { login: process.env.ADMIN_USER, password: process.env.ADMIN_PASSWORD }
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    if (login && password && login === auth.login && password === auth.password) {
+      return next()
+    }
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send('Authentication required.')
+  })
 
   app.use(express.json())
   
@@ -75,21 +92,9 @@ if (cluster.isWorker) {
     return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
   }
 
-  app.use((_, res, next) => {
-    const start = process.hrtime()
-    res.on('close', _ => {
-        const durationInMilliseconds = getDurationInMilliseconds(start)
-        log(`--- Function ended after ${durationInMilliseconds.toLocaleString()} ms`)
-    })
-    next()
-  })
-
   const authenticateToken = (req, res, next) => {
     const failure = { state: false }
-    if (!useAuth) {
-      next()
-      return
-    }
+    if (!useAuth) return next()
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (!token) {
@@ -104,18 +109,36 @@ if (cluster.isWorker) {
         return res.status(200).send({ error: 'Token did not verify'})
       }
       req.user = user
-      next()
+      return next()
     })
   }
   
+  app.use((req, res, next) => {
+    if (!req.originalUrl.includes('/function/')) return next()
+    const start = process.hrtime()
+    res.on('close', _ => {
+        const durationInMilliseconds = getDurationInMilliseconds(start)
+        const message = `--- Function ended after ${durationInMilliseconds.toLocaleString()} ms`
+        log(message)
+        Array.from(sockets.keys()).forEach(socket => {
+          socket.send(message.replace('---', '') + '<br>\n')
+        })
+    })
+    return next()
+  })
+
   for (let f = 0; f < functions.length; f++) {
     app.post(`/function/${functions[f].name}`, authenticateToken, ((req, res) => {
       const func =
         require(`${functionsPath}/${functions[f].name}/index`)
       functions[f].counter += 1
-      log(`--- Invoking function "${functions[f].name}" (${functions[f].counter})`)
+      const message = `--- Invoking function "${functions[f].name}" (${functions[f].counter})`
+      log(message)
       try {
         func(req.body, res)
+        Array.from(sockets.keys()).forEach(socket => {
+          socket.send(new Date(Date.now()).toString().replace(/\((.+)\)/, '') + message)
+        })
       } catch(err) {
         log(err.message)
         res.status(500).send()
@@ -123,9 +146,24 @@ if (cluster.isWorker) {
     }))
   }
   
-  app.listen(API_PORT, _ => log(`--- ${functions.length} function${
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'public')))
+
+  wss.on('connection', socket => {
+    console.log('--- websocket connection')
+    sockets.set(socket, {})
+    socket.on('close', _ => sockets.delete(socket))
+  })
+
+  const server = app.listen(API_PORT, _ => log(`--- ${functions.length} function${
     functions.length != 1 ? 's' : ''
   } active`))
+  
+  server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, socket => {
+      wss.emit('connection', socket, request)
+    })
+  })
 
 }
 
